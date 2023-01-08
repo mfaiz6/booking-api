5649370c3a19b602d4d88f857a9add05f7a16835
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-08 13:53:26 +0530

    initiate connection to MongoDB using mongoose

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 import express from 'express'
 const app = express()
 
+import dotenv from "dotenv"
+dotenv.config()
 
 
+import mongoose from 'mongoose'
+
+//initial connection to mongoDB
+const connect = () => {
+    try {
+        mongoose.set("strictQuery", false)
+        mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to mongoDB");
+    }
+    catch (error) {
+        throw (error)
+    }
+}
+
+
+//check if connected or not, and take action acordingly
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB down!!");
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB up!")
+})
 
 
 
 
 app.listen(8800, () => {
+    connect()
     console.log("Connected to backend!")
 })
